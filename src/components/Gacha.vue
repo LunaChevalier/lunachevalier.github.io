b808ac616a020a6c6ab099cc343ab2a42d42edc9
LunaChevalier <luna.chevalier.817.s@gmail.com>
2020-08-26 17:30:59 +0900

    ボタンを押したら、具材がランダム抽選されるように修正

--- src/components/Gacha.vue
+++ src/components/Gacha.vue
@@ -1,11 +1,37 @@
 <template>
-  <p>HOME VUE</p>
+  <div class="gacha">
+    <button @click="lottery()">push</button>
+    <GachaResult :ingres="ingres" ></GachaResult>
+  </div>
 </template>
 
 <script lang='ts'>
   import { Component, Vue } from 'vue-property-decorator';
+  import GachaResult from '@/components/GachaResult.vue';
 
-  @Component
+  @Component({
+    components: {
+      GachaResult,
+    },
+    
+  })
+
   export default class Gacha extends Vue {
+    data() {
+      return {
+        ingres: [],
+      }
+    }
+    private lottery() {
+      const ingres: string[] = ['玉ねぎ', '人参', 'じゃがいも'];
+      const out = Array.from(ingres);
+      for (let i = out.length - 1; i > 0; i--) {
+        const r = Math.floor(Math.random() * (i + 1));
+        const tmp = out[i];
+        out[i] = out[r];
+        out[r] = tmp;
+      }
+      this.ingres = out
+    }
   }
 </script>
