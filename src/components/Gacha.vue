0666679ec58edf15ed728ea359e826ef8a29aa73
LunaChevalier <luna.chevalier.817.sk@gmail.com>
2020-09-03 17:34:12 +0900

    抽選対象になる具材を表示

--- src/components/Gacha.vue
+++ src/components/Gacha.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="gacha">
+    <TargetIngredients :targetIngredients="targetIngredients"></TargetIngredients>
     <label class="text-nowrap">具の数:</label><input class="count m-2" v-model="count">
     <button class="lottery btn btn-primary m-2" @click="lottery()">具材を決定</button>
     <GachaResult :ingredient="ingredients" ></GachaResult>
@@ -9,22 +10,26 @@
 <script lang='ts'>
   import { Component, Vue } from 'vue-property-decorator';
   import GachaResult from '@/components/GachaResult.vue';
+  import TargetIngredients from '@/components/TargetIngredients.vue';
   import Ingredient from '@/types/Ingredient';
   import ingredient from '@/assets/json/ingredients.json';
 
   @Component({
     components: {
       GachaResult,
+      TargetIngredients,
     },
   })
 
   export default class Gacha extends Vue {
     protected count!: number;
     protected ingredients!: string[];
+    protected targetIngredients!: string[];
     protected data() {
       return {
         ingredients: [],
         count: 1,
+        targetIngredients: Array.from(ingredient.map((ingre) => ingre.name)), 
       };
     }
     private lottery() {
---
+++ src/components/TargetIngredients.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="target-ingredients">
+    <h2>抽選対象になる具材</h2>
+    <label class="target-ingredient border rounded border-info p-1 m-2" v-for="ingre in targetIngredients">
+      {{ingre}}
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType }  from 'vue';
+
+export default Vue.extend({
+  props: {
+    targetIngredients: {
+      type: Array as PropType<string[]>,
+    },
+  },
+});
+</script>
