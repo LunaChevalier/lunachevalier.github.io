262e599b1bee3784853493e954b36f3a637911c5
LunaChevalier <luna.chevalier.817.sk@gmail.com>
2020-09-10 17:35:26 +0900

    ガチャの結果にアニメーションを追加

--- src/components/Gacha.vue
+++ src/components/Gacha.vue
@@ -2,7 +2,8 @@
   <div class="gacha">
     <TargetIngredients :targetIngredients="targetIngredients"></TargetIngredients>
     <label class="text-nowrap">具の数:</label><input class="count m-2" v-model="count">
-    <button class="lottery btn btn-primary m-2" @click="lottery()" :disabled="!(Number.isInteger(parseInt(count, 10))) || count < 1">具材を決定</button>
+    <label class="lottery btn btn-primary m-2" for="toggle" @click="lottery()" :disabled="!(Number.isInteger(parseInt(count, 10))) || count < 1">{{isCheck ? "もとに戻す" : "具材を決定"}}</label>
+    <input type="checkbox" id="toggle" v-model="isCheck">
     <GachaResult :ingredients="ingredients" ></GachaResult>
   </div>
 </template>
@@ -25,6 +26,7 @@
     protected count!: number;
     protected ingredients!: Ingredient[];
     protected targetIngredients!: Ingredient[];
+    protected isCheck = false;
 
     protected data() {
       return {
@@ -35,6 +37,7 @@
     }
 
     private lottery() {
+      if (this.isCheck) return;
       const out = ingredient;
       const count = out.length > this.count ? this.count : out.length;
       for (let i = 0; i < count; i++) {
@@ -48,3 +51,7 @@
     }
   }
 </script>
+
+<style module lang='less'>
+  @import '../assets/less/gacha.less';
+</style>
--- src/components/GachaResult.vue
+++ src/components/GachaResult.vue
@@ -1,12 +1,10 @@
 <template>
-  <!-- TODO アニメーションを付けたいが、うまく作動しないので後で実装 -->
-  <transition name="gacha-result" mode="out-in">
-    <div>
-      <label class="gacha-ingredient border rounded border-info p-1 m-2" v-for="ingre in ingredients" :key="ingre.name">
-        <IngredientVue :name="ingre.name" :categories="ingre.categories"></IngredientVue>
-      </label>
-    </div>
-  </transition>
+
+  <div class="gacha-result">
+    <label class="gacha-ingredient border rounded border-info p-1 m-2" v-for="ingre in ingredients" :key="ingre.name">
+      <IngredientVue :name="ingre.name" :categories="ingre.categories"></IngredientVue>
+    </label>
+  </div>
 </template>
 
 <script lang="ts">
